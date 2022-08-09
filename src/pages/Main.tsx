414392350e1698f9d410942f5935254a6d0eb098
Lucas <lucas.moreno.olivos@gmail.com>
2022-08-09 13:30:45 -0500

    feat: filter pokemons by name and id

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,6 +9,7 @@ import "./styles/main.css"
 const Main = () => {
 
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([])
 
   const getPokemons = async () => {
     const pokemonApi = PokemonApi.Instance
@@ -23,19 +24,40 @@ const Main = () => {
       console.error(error)
     }
   }
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    const filtered = pokemons.filter((pokemon : Pokemon) => {
+      return pokemon.name.includes(value) || pokemon.number.toString().includes(value)
+    })
+    setFilteredPokemons(filtered)
+    
+  }
+
   useEffect(() => {
     getPokemons()
+    setFilteredPokemons(pokemons)
   }, [])
 
   return (
-    <section className="mainGrid">
-      {pokemons.map((pokemon : Pokemon, index : number) => (
-          <PokemonCard
-            key={`${pokemon.name}-${index+1}`}
-            name={pokemon.name}
-            number={pokemon.number}
-          />
-      ))}
+    <section>
+      <div>
+        <input 
+          type="text" 
+          className="inputMain" 
+          placeholder="Que pokemon buscas"
+          onChange={handleOnChange}
+        />
+      </div>
+      <div className="mainGrid">
+        {filteredPokemons.map((pokemon : Pokemon, index : number) => (
+            <PokemonCard
+              key={`${pokemon.name}-${index+1}`}
+              name={pokemon.name}
+              number={pokemon.number}
+            />
+        ))}
+      </div>
     </section>
   );
 }
